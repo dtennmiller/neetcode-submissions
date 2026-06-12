7d17ce7cb6ef70c96e02a7740a3d7a92c32b05bd
dtennmiller <86261343+dtennmiller@users.noreply.github.com>
2026-06-12 08:44:52 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::unordered_map<char, int> map1;
+        std::unordered_map<char, int> map2;
+        for (char c : s)
+        {
+            map1[c]++;
+        }
+
+        for (char c : t)
+        {
+            map2[c]++;
+        }
+
+        if (map1 == map2)
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+};
